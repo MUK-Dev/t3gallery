4057a329a3230ec2aecae16c9de288965da85722
Muhammad Usman Khilji <manian1221@gmail.com>
2024-07-11 17:20:41 +0500

    Added sentry and parallel routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -22,13 +23,15 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((img) => (
         <div key={img.id} className="flex w-48 flex-col">
-          <Image
-            src={img.url}
-            alt={img.url}
-            style={{ objectFit: "contain" }}
-            width={192}
-            height={192}
-          />
+          <Link href={`/img/${img.id}`}>
+            <Image
+              src={img.url}
+              alt={img.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+            />
+          </Link>
           <div className="truncate">{img.name}</div>
         </div>
       ))}

f0faa4cc240bd4c2d70810f4ac1bec8eca8f70b6
Muhammad Usman Khilji <manian1221@gmail.com>
2024-07-11 17:57:57 +0500

    Fixed dialog full screen, made component for image view

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,17 +1,17 @@
-import { getImage } from "~/server/queries";
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
   const idAsNumber = Number(photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo ID");
-  const image = await getImage(idAsNumber);
 
   return (
-    <div>
-      <img src={image.url} className="w-full" />
-    </div>
+    <Modal>
+      <FullPageImageView id={idAsNumber} />
+    </Modal>
   );
 }
